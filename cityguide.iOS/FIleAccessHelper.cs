1e837df2ce73f479af399ccd82dbaee4c860a0d2
niall247 <58305045+niall247@users.noreply.github.com>
2019-11-28 20:11:56 +0000

    Added IOS code

---
+++ cityguide.iOS/FIleAccessHelper.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.IO;
+using Foundation;
+
+namespace cityguide.iOS
+{
+    public class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string filename)
+        {
+
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            string dbPath = Path.Combine(libFolder, filename);
+
+            CopyDatabaseIfNotExists(dbPath);
+
+            return dbPath;
+        }
+
+        private static void CopyDatabaseIfNotExists(string dbPath)
+        {
+
+
+                var existingDb = NSBundle.MainBundle.PathForResource("madrid", "db");
+                File.Delete(dbPath);
+                File.Copy(existingDb, dbPath);
+           
+        }
+    }
+}
+
+
